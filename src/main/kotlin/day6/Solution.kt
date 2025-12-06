79e37ee535e2c3e8a82c65b646ee4c66c5d9ca34
Rasmus <rasmus.raasuke@gmail.com>
2025-12-06 22:27:28 +0200

    Day 6 Solution (part 2)

--- src/main/kotlin/day6/Solution.kt
+++ src/main/kotlin/day6/Solution.kt
@@ -2,19 +2,37 @@ package day6
 
 fun solution(input: String): Long {
     val lines = input.lines()
-    val symbols = lines[lines.lastIndex].trim().split(Regex("\\s+"))
-
-    val results = mutableMapOf<Int, Long>()
-    for (line in lines.take(lines.size - 1)) {
-        for ((index, num) in line.trim().split(Regex("\\s+")).withIndex()) {
-            val long = num.toLong()
-            when (symbols[index]) {
-                "+" -> results[index] = results.getOrPut(index) {0} + long
-                "*" -> results[index] = results.getOrPut(index) {1} * long
-            }
-
-        }
+    val maxLen = lines.maxOf { it.length }
+    val padded = lines.map { it.padEnd(maxLen) }
+    val transposed = (0 until maxLen).map { i ->
+        padded.joinToString("") { it[i].toString() }
     }
 
-    return results.values.sum()
+    val results = mutableListOf<Long>()
+
+    var num = 0L
+    var operation = ""
+    for (value in transposed) {
+        if (value.isBlank()) {
+            results.add(num)
+            num = 0L
+        } else if (num == 0L) {
+            val parts = value.trim().split(Regex("\\s+"))
+            if (parts.size == 2) {
+                num = parts[0].toLong()
+                operation = parts[1]
+            } else {
+                num = value.trim().dropLast(1).toLong()
+                operation = value[value.lastIndex].toString()
+            }
+        } else {
+            when (operation) {
+                "+" -> num += value.trim().toLong()
+                "*" -> num *= value.trim().toLong()
+            }
+        }
+    }
+    results.add(num)
+
+    return results.sum()
 }
