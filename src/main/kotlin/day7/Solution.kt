3c0610eb571c63edfd3a5a77cfdb5c4ae53f5d12
Rasmus <rasmus.raasuke@gmail.com>
2025-12-09 21:47:03 +0200

    Day 7 Solution (part 2)

--- src/main/kotlin/day7/Solution.kt
+++ src/main/kotlin/day7/Solution.kt
@@ -1,34 +1,37 @@
 package day7
 
-fun solution(input: String): Int {
-    val lines = input.lines().toMutableList()
-    var timesSplit = 0
+fun solution(input: String): Long {
+    val matrix = input.lines().map { line -> line. map { it.toString() }.toMutableList()}
 
-    for ((i, line) in lines.withIndex()) {
-        if (line.contains("S")) {
-            val pos = line.indexOfFirst { it == 'S' }
-            lines[i+1] = lines[i+1].replaceAt(pos, '|')
-        } else if (line.contains("|") && i != lines.lastIndex) {
+    for ((i, line) in matrix.withIndex()) {
+        if (i == 0) {
+            val pos = line.indexOfFirst { it == "S" }
+            matrix[1][pos] = "1"
+        } else if (i != matrix.lastIndex) {
             val indexes = line.mapIndexedNotNull { index, c ->
-                if (c == '|') index else null
+                if (c != "." && c != "^") index else null
             }
             for (index in indexes) {
-                when (lines[i+1][index]) {
-                    '.' -> lines[i+1] = lines[i+1].replaceAt(index, '|')
-                    '^' -> {
-                        lines[i+1] = lines[i+1].replaceAt(index - 1, '|')
-                        lines[i+1] = lines[i+1].replaceAt(index + 1, '|')
-                        timesSplit++
+                when (matrix[i + 1][index]) {
+                    "." -> matrix[i + 1][index] = line[index]
+                    "^" -> {
+                        // Left side
+                        var leftSum = line[index].toLong() + (line[index - 1].toLongOrNull() ?: 0)
+                        if (index - 2 >= 0 && matrix[i + 1][index - 2] == "^") {
+                            leftSum += line[index - 2].toLongOrNull() ?: 0
+                        }
+                        matrix[i + 1][index - 1] = leftSum.toString()
+
+                        // Right side
+                        var rightSum = line[index].toLong() + (line[index + 1].toLongOrNull() ?: 0)
+                        if (index + 2 <= line.lastIndex && matrix[i + 1][index + 2] == "^") {
+                            rightSum += line[index + 2].toLongOrNull() ?: 0
+                        }
+                        matrix[i + 1][index + 1] = rightSum.toString()
                     }
                 }
             }
-
         }
-        println(line)
     }
-    return timesSplit
-}
-
-fun String.replaceAt(index: Int, ch: Char): String {
-    return StringBuilder(this).also { it[index] = ch }.toString()
+    return matrix.last().sumOf { s -> s.toLongOrNull() ?: 0 }
 }
